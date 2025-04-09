84fafd3cf5211b7c822aa92b58feb9ad814dcc8d
Bhavarth <abcbhavarth@gmail.com>
2025-04-09 20:00:01 +0530

    Completed  matplotlib practical 7

---
+++ pr7.py
@@ -0,0 +1,33 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from random import random
+
+# a
+x = np.arange(1, 50)
+y = x*3
+
+plt.subplot(2,2,1)
+plt.title("Line Plot")
+plt.plot(x,y)
+
+languages =  ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
+popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
+colors =  ["#1f77b4", "#ff7f0e", "#2ca02c",
+"#d62728", "#9467bd", "#8c564b"]
+
+plt.subplot(2,2,2)
+plt.title("Bar Chart")
+plt.bar(languages,popularity)
+
+plt.subplot(2,2,3)
+plt.title("Pie Chart")
+plt.pie(popularity, labels=languages, colors=colors)
+
+plt.subplot(2,2,4)
+numsX = np.random.randint(10, 500, 200)
+numsY = np.random.randint(10, 500, 200)
+plt.title("Scatter Plot")
+plt.scatter(numsX, numsY)
+
+plt.tight_layout()
+plt.show()
