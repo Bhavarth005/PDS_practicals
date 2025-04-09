db321cd8bda28d5f117b139043abe0e90fe5041e
Bhavarth <abcbhavarth@gmail.com>
2025-04-09 20:01:12 +0530

    minor tweaks

--- pr7.py
+++ pr7.py
@@ -10,19 +10,22 @@ plt.subplot(2,2,1)
 plt.title("Line Plot")
 plt.plot(x,y)
 
+# b
 languages =  ['Java', 'Python', 'PHP', 'JavaScript', 'C#', 'C++']
 popularity = [22.2, 17.6, 8.8, 8, 7.7, 6.7]
-colors =  ["#1f77b4", "#ff7f0e", "#2ca02c",
-"#d62728", "#9467bd", "#8c564b"]
 
 plt.subplot(2,2,2)
 plt.title("Bar Chart")
 plt.bar(languages,popularity)
 
+#c 
+colors =  ["#1f77b4", "#ff7f0e", "#2ca02c",
+"#d62728", "#9467bd", "#8c564b"]
 plt.subplot(2,2,3)
 plt.title("Pie Chart")
 plt.pie(popularity, labels=languages, colors=colors)
 
+#d
 plt.subplot(2,2,4)
 numsX = np.random.randint(10, 500, 200)
 numsY = np.random.randint(10, 500, 200)
